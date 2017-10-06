66fcc6b6d52ec0b3332a9ebe1b47b44c1dd80a52
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-06 15:44:13 -0500

    add youtube api search function

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD6PTFG636crwR350f0EHTiiqqZBmXYxG8';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data));
 
 const App = () => {
   return (
