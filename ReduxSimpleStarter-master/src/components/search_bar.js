bd1b28fd620109992fafa57a8a8ae2414186cb6b
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-06 10:35:41 -0500

    add value into input to fix error

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -11,8 +11,10 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        value={this.state.term}
-        <input onChange={event => this.setState({ term: event.target.value })} />
+        <input 
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
       </div>
     );
   }
