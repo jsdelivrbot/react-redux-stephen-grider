1d8c42196d9f4f69d656acbf3c1afb1220fc46a8
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-05 19:34:28 -0500

    install youtube search package and save API KEY in index.js file

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyD6PTFG636crwR350f0EHTiiqqZBmXYxG8';
+
 const App = () => {
   return (<div>Hi there!</div>);
 }
