c670f5327002d5e2b3fab343dd6061c5c56fd422
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-08 18:33:52 -0500

    add selectedVideo and render instead of hardcoded one

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -11,10 +11,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0] 
+      });
     });
   }
 
@@ -22,7 +28,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
