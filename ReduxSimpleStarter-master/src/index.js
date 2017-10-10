dc0e97fe29f3ad340397d939c3ec87a53d89061f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-09 23:12:31 -0500

    add video search function to SearchBar component

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -22,12 +22,23 @@ class App extends Component {
         selectedVideo: videos[0]
       });
     });
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
