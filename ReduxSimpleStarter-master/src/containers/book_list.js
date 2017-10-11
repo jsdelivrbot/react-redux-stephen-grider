ed4ac4d9bf6b2d2f6f46d3c923d7491bafb1d130
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-11 11:08:26 -0500

    create an action and bind to BookList container

--- ReduxSimpleStarter-master/src/containers/book_list.js
+++ ReduxSimpleStarter-master/src/containers/book_list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 
@@ -24,4 +26,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+/* Anything returned from this function will end up as props on the
+BookList container */
+function mapDispatchToProps(dispatch) {
+  /*Whenever selectBook is called, the result should be passed to all of
+  our reducers */
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
