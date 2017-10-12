800bd0cfdceed8f8b192b4dd3421bfea5595ca4b
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-12 12:55:34 -0500

    handle form input with a callback function when user types anything

--- ReduxSimpleStarter-master/src/containers/search_bar.js
+++ ReduxSimpleStarter-master/src/containers/search_bar.js
@@ -10,15 +10,21 @@ export default class SearchBar extends Component {
   }
 
   onInputChange = e => {
-    console.log(e.target.value);
     this.setState({
       term: e.target.value
     });
   }
 
+  onFormSubmit = e => {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form
+        onSubmit={this.onFormSubmit}
+        className="input-group"
+      >
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
