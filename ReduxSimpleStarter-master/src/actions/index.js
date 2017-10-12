6263806792394fda9bdbb3ea3bca10276c39ea9e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-12 16:46:45 -0500

    install AJAX request with axios

--- ReduxSimpleStarter-master/src/actions/index.js
+++ ReduxSimpleStarter-master/src/actions/index.js
@@ -1,1 +1,10 @@
 const API_KEY = 'd744afe3260cc49bff44102ec131e0d1';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  };
+}
