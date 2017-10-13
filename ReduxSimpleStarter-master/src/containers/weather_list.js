4780cecb4c385588fe9a97b8b5dbea50369ec07e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-13 12:53:44 -0500

    add humidity and pressure charts

--- ReduxSimpleStarter-master/src/containers/weather_list.js
+++ ReduxSimpleStarter-master/src/containers/weather_list.js
@@ -8,13 +8,15 @@ class WeatherList extends Component {
     const name = cityData.city.name;
     //accessing data to use sparkline charts
     const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressures} color="green" /></td>
+        <td><Chart data={humidities} color="blue" /></td>
       </tr>
     );
   }
