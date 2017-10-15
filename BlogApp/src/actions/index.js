deae7e7002a0c7096178d445d036ad3b721135f1
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-15 15:49:53 -0500

    fix typos and syntax in reducer and connect PostsIndex container to redux

--- BlogApp/src/actions/index.js
+++ BlogApp/src/actions/index.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'http://reduxblog.rerokuapp.com/api';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=Whatever12345';
 
 export function fetchPosts() {
-  const request = axios.get(`$(ROOT_URL)/posts${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
