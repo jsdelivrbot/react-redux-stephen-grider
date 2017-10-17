d3c183fa565176ae6eebe6654d10a87563dcba12
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-17 09:46:06 -0500

    handle form submittal

--- BlogApp/src/components/posts_new.js
+++ BlogApp/src/components/posts_new.js
@@ -17,24 +17,32 @@ class PostsNew extends Component {
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title For Post"
           name="title"
           component={this.renderField}
         />
-      <Field
-        label="Categories"
-        name="categories"
-        component={this.renderField}
-      />
-    <Field
-      label="Post Content"
-      name="content"
-      component={this.renderField}
-    />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      <button type="submit" className="btn btn-primary">Submit</button>
+
       </form>
     );
   }
