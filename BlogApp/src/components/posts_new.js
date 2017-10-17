5e3783d3e2c2539e41e3b3f032810fac2948047a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-17 09:04:55 -0500

    validate form

--- BlogApp/src/components/posts_new.js
+++ BlogApp/src/components/posts_new.js
@@ -20,17 +20,17 @@ class PostsNew extends Component {
     return (
       <form>
         <Field
-          label="Title"
+          label="Title For Post"
           name="title"
           component={this.renderField}
         />
       <Field
-        label="Text"
-        name="tags"
+        label="Categories"
+        name="categories"
         component={this.renderField}
       />
     <Field
-      label="Content"
+      label="Post Content"
       name="content"
       component={this.renderField}
     />
@@ -39,6 +39,27 @@ class PostsNew extends Component {
   }
 }
 
+const validate = values => {
+  // console.log(values); -> { title: 'fdf', categories: 'gdfg', content: 'rthur' }
+  const errors = {};
+
+  //Validate the inputs from 'values'
+  if(!values.title || values.title.length < 3) {
+    errors.title = 'Please enter a title that is at least 3 characters!';
+  }
+  if(!values.categories) {
+    errors.categories = 'Enter a category!';
+  }
+  if(!values.content) {
+    errors.content = 'Enter a content!';
+  }
+
+  //If errors is empty, the form is fine to submit
+  //If errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
