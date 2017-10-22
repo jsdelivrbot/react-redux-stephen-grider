91c6b9ee1baecc5240391dd3a01e743be6bb32b3
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-21 19:27:17 -0500

    create a reducer to receive new post

--- BlogApp/src/components/posts_show.js
+++ BlogApp/src/components/posts_show.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+
   render() {
     return (
       <div>Posts Show Component</div>
@@ -8,4 +11,4 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow;
+export default connect(null, { fetchPost })(PostsShow);
