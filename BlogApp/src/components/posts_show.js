77f99c0ee5ccee8e4ea9ec59fb9a95e4a2dc6816
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-21 19:05:44 -0500

    create Posts Show component and add a route in default index page

---
+++ BlogApp/src/components/posts_show.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return (
+      <div>Posts Show Component</div>
+    );
+  }
+}
+
+export default PostsShow;
