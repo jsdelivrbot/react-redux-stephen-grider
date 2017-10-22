5309ee57da83b8ca4db317266586d57dda69ddb4
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-21 21:42:26 -0500

    add mapStateToProps function with ownProps arguments to fetch one Post

--- BlogApp/src/components/posts_show.js
+++ BlogApp/src/components/posts_show.js
@@ -4,11 +4,21 @@ import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    // posts[this.props.match.params.id]; //the post we want to show
     return (
       <div>Posts Show Component</div>
     );
   }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps({ posts }, ownProps) { //ownProps would be this.props
+  return { post: posts[ownProps.match.params.id] }; //the post we want to show
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
