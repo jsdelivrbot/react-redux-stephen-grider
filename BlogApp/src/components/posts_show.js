d6e0a5cb576fc8916dc529bca41b249d05e77c12
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-24 14:21:19 -0500

    add delete pot functionality and navigate user back to default page afterwards

--- BlogApp/src/components/posts_show.js
+++ BlogApp/src/components/posts_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
 
@@ -10,6 +10,13 @@ class PostsShow extends Component {
       const { id } = this.props.match.params;
       this.props.fetchPost(id);
     }
+  }
+
+  handleDeletePost() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -23,6 +30,12 @@ class PostsShow extends Component {
     return (
       <div>
         <Link className="btn btn-primary" to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger xs-pull-right"
+          onClick={this.handleDeletePost.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -35,4 +48,4 @@ function mapStateToProps({ posts }, ownProps) { //ownProps would be this.props
   return { post: posts[ownProps.match.params.id] }; //the post we want to show
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
